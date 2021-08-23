aa10a0adcd8bea7dba1eefd8fcfa5b2abca7a566
johnMinelli <giovanni.minelli3@gmail.com>
2021-08-23 16:43:53 +0200

    revised and working, pre clean up

--- rendering.py
+++ rendering.py
@@ -1,71 +1,39 @@
-﻿from scipy.spatial import HalfspaceIntersection, ConvexHull
-import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+﻿import matplotlib
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d    
+# https://github.com/Malkovsky/interactive-visualization/blob/master/examples/overview.ipynb
 
-def render_inequalities(halfspaces, feasible_point, xlim, ylim):
-    hs = HalfspaceIntersection(np.array(halfspaces), np.array(feasible_point))
-    fig, ax = plt.subplots()
-    ax.set_xlim(xlim)
-    ax.set_ylim(ylim)
+def render_search_results(optimal_solutions):
 
-    x = np.linspace(*xlim, 100)
+    sols = np.array(optimal_solutions)
+    t = -1
+    for sol in sols:
+        if sol[2] == t: sol[2] += 10000
+        t = sol[2]
 
-    for h in halfspaces:
-        if h[1]== 0:
-            ax.axvline(-h[2]/h[0], color="#2c3e50")
-        else:
-            ax.plot(x, (-h[2]-h[0]*x)/h[1], color="#2c3e50")
-    x, y = zip(*hs.intersections)
-    points = list(zip(x, y))
-    convex_hull = ConvexHull(points)
-    polygon = Polygon([points[v] for v in convex_hull.vertices], color="#34495e")
-    ax.add_patch(polygon)
-    ax.plot(x, y, 'o', color="#e67e22")
+    sizes = sols[:, 0]
+    values = sols[:, 1]
+    temps = sols[:, 2]
+    color_map = 'coolwarm_r'
+    cm = plt.get_cmap(color_map)
+    n_sol = len(sizes)
+    plt.close()
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.set_xticks(range(np.int(min(sizes)) if min(sizes)!=max(sizes) else np.int(min(sizes))-1, np.int(max(sizes))+1))
+    ax.bar3d(sizes, temps, -np.zeros_like(sizes), 0.15, 25000, values, shade=True,
+             color=[cm(1.0 * i / (n_sol - 1)) for i in range(n_sol)])
 
-def printTableau(self, tableau):
-
-    print("ind \t\t", end="")
-    for j in range(0, len(c)):
-        print("x_" + str(j), end="\t")
-    for j in range(0, (len(tableau[0]) - len(c) - 2)):
-        print("s_" + str(j), end="\t")
-
-    print()
-    for j in range(0, len(tableau)):
-        for i in range(0, len(tableau[0])):
-            if (not np.isnan(tableau[j, i])):
-                if (i == 0):
-                    print(int(tableau[j, i]), end="\t")
-                else:
-                    print(round(tableau[j, i], 2), end="\t")
-            else:
-                print(end="\t")
-        print()
-
-def getTableau(self):
-
-    t1 = np.array([None, 0])
-    numVar = len(self.c)
-    numSlack = len(self.A)
-
-    t1 = np.hstack(([None], [0], self.c, [0] * numSlack))
-
-    basis = np.array([0] * numSlack)
-
-    for i in range(0, len(basis)):
-        basis[i] = numVar + i
-
-    A = self.A
-
-    if (not ((numSlack + numVar) == len(self.A[0]))):
-        B = np.identity(numSlack)
-        A = np.hstack((self.A, B))
-
-    t2 = np.hstack((np.transpose([basis]), np.transpose([self.b]), A))
-
-    tableau = np.vstack((t1, t2))
-
-    tableau = np.array(tableau, dtype='float')
-    
-    return tableau
+    for i in range(n_sol):
+        ax.set_prop_cycle("c", [cm(1.0 * i / (n_sol - 1)) for i in range(n_sol - 1)])
+        for i in range(n_sol - 1):
+            plt.plot(sizes[i:i + 2], temps[i:i + 2], values[i:i + 2])
+    ax.set_xlabel('N° grids')
+    ax.set_ylabel('Temperature')
+    ax.set_zlabel('Optimal cost')
+    ax.set_title('SISA Search evaluation')
+    plt.show()
+    plt.close()
--- simplex.py
+++ simplex.py
@@ -1,13 +1,7 @@
 ﻿import math
-from decimal import *
-from fractions import Fraction
-
-import numpy
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-from rendering import render_inequalities
 
 '''
 # We will consider only linear program in equational form such that
@@ -48,45 +42,74 @@ Maximize cᵀx subject to Ax ≤ b and x≥0      (P) is equal to     Minimize b
 
 class SIMPLEX:
     # [[3 3 0], [1 0 2], [0 1 2]]
-    def __init__(self):
+    def __init__(self, debug=False):
+        self.debug = debug
         self.TOL = 1.0E-12
 
-    #     # c = [1, 1, 1, 0, 0, 0, 0, 0]
-    #     # A = [[2, 1, 0, -1, 0, 0, 0, 0],
-    #     #      [0, 2, 0, 0, -1, 0, 0, 0],
-    #     #      [2, 0, 1, 0, 0, -1, 0, 0],
-    #     #      [0, 1, 1, 0, 0, 0, -1, 0],
-    #     #      [0, 0, 2, 0, 0, 0, 0, -1]]
-    #     # b = [84500, 94500, 103500, 114500, 135500]
-    #     c = [1, 1]
-    #     # A = [[0, 1],
-    #     #      [4, 0],
-    #     #      [0, 3]]
-    #     # A = [[0, 1],
-    #     #      [0, 2],
-    #     #      [4, 1]]
-    #     A = [[0, 2],
-    #          [3, 1],
-    #          [1, 1]]
-    #     # A = [[3, 1, 0],
-    #     #      [0, 2, 0],
-    #     #      [1, 1, 4]]
-    #     b = [4500, 9000, 16000]
-    #     getcontext().prec = 4
-    #     halfspaces = [
-    #         [-1, 0, 0],  # x₁ ≥ 0
-    #         [0, -1, 0],  # x₂ ≥ 0
-    #         [-1, 1, -2],  # -x₁ + x₂ ≤ 2
-    #         [1, 0, -4],  # x₁ ≤ 4
-    #         [0, 1, -4],  # x₂ ≤ 4
-    #     ]
-    #     feasible_point = np.array([0.5, 0.5])
-    # 
-    #     try:
-    #         solution = self.solve(c, A, b, halfspaces, feasible_point, show=True, debug=True)
-    #         print('Optimal solution: ', solution)
-    #     except Exception as e:
-    #         print(e)
+        # c = [1, 1, 1, 0, 0, 0, 0, 0]
+        # A = [[2, 1, 0, -1, 0, 0, 0, 0],
+        #      [0, 2, 0, 0, -1, 0, 0, 0],
+        #      [2, 0, 1, 0, 0, -1, 0, 0],
+        #      [0, 1, 1, 0, 0, 0, -1, 0],
+        #      [0, 0, 2, 0, 0, 0, 0, -1]]
+        # b = [84500, 94500, 103500, 114500, 135500]
+        c = [1, 1]
+        # A = [[0, 1],
+        #      [4, 0],
+        #      [0, 3]]
+        # A = [[0, 1],
+        #      [0, 2],
+        #      [4, 1]]
+        A = [[0, 2],
+             [3, 1],
+             [1, 1]]
+        # A = [[3, 1, 0],
+        #      [0, 2, 0],
+        #      [1, 1, 4]]
+        b = [4500, 9000, 16000]
+        
+        # example from geekrodion
+        # /*A=[[-1, 1, 1, 0], [ 1, 0, 0, 1]]
+        # b = [0, 2]
+        # c = [0, 1, 0, 0]*/
+        
+        # pre normalization it raised an unbounded
+        # A=[[0,1,0,1,0,0],
+        #     [1,0,0,0,0,0],
+        #     [0,0,0,0,1,0],
+        #     [0,0,0,0,1,0],
+        #     [0,1,0,0,0,1],
+        #     [1,0,0,0,0,0],
+        #     [0,0,0,0,0,1],
+        #     [0,0,1,1,0,1],
+        #     [0,1,1,1,1,0],
+        #     [1,0,1,0,0,0],
+        #     [0,1,1,1,0,0],
+        #     [1,0,0,0,1,1]]
+        # b = [1000, 1500, 2000, 2000, 2500, 3000, 4500, 5200, 6000, 7000, 9000, 12000]
+        # c = [1.,1.,1.,1.,1.,1.]
+
+        A=[[0,0,1,0,0,0,0],
+            [0,0,0,0,1,0,0],
+            [1,0,0,0,1,0,0],
+            [0,0,0,0,1,0,0],
+            [0,0,1,0,0,0,0],
+            [1,0,0,0,0,0,1],
+            [0,0,0,1,0,1,1],
+            [0,0,1,0,0,1,0],
+            [0,1,1,0,0,0,1],
+            [2,1,0,1,0,0,0],
+            [0,1,0,1,1,1,0],
+            [0,1,0,1,0,1,1]]
+        b = [1000, 1500, 2000, 2000, 2500, 3000, 4500, 5200, 6000, 7000, 9000, 12000]
+        c = [1.,1.,1.,1.,1.,1.,1.]
+
+
+        try:
+            solution = self.solve(c, A, b, render=True)
+            print('Optimal solution: ', solution)
+        except Exception as e:
+            print(e)
 
     def phase1_tableau(self, c, A, b):
         vars = len(c)
@@ -111,31 +134,28 @@ class SIMPLEX:
         # moreover add an artificial variable for each constraint without slack variable
         av = np.identity(n_cons)
         A = np.hstack((A, av))
-        a_vars = np.arange(len(A[0])-len(av[0]), len(A[0]))
+        a_vars = np.arange(len(A[0]) - len(av[0]), len(A[0]))
         # artificial variables are also the basic variables
         basics = a_vars.copy()
 
-        # the problem objective is a minimization so the obj function is turned into a maximization and completed with 0
-        c = -np.hstack((c, [0] * len(surplus[0]), [0] * len(av[0])))
-        
+        # the problem objective is a minimization. Turn it in max like that:  min Z=a1+a2 -> max Z=-a1-a2 -> Z+a1+a2=0
+        # rearrange changing sign Z + x_1 + x_2 = 0  ; then complete with 0s
+        c = np.hstack((c, [0] * len(surplus[0]), [0] * len(av[0])))
+
         return c, A, b, a_vars, basics
 
     def to_tableau_first_phase(self, c, A, b, a_vars, basics):
         xb = np.column_stack((A, b))
         # original objective function
         z = np.hstack((c, 0))
-        # new objective function is a summation of artificial variables
+        # new obj function is a min of a sum of artificial var. Turn into max:  min Z=sum ai -> max Z=sum -ai -> Z+ai=0
         z1 = np.zeros(len(z))
-        z1[a_vars] = -1  # transformed into a maximization
+        z1[a_vars] = 1  # transformed into a maximization
         # then expressed in form of non basic variables
-        z1 = z1 + np.sum(xb[np.any(xb[:, a_vars] > 0, axis=1)], axis=0)  # sum of constraints where there is an a_var
+        # sum of constraints where there is an a_var and subtract to make  'np.any(tab[-1, basics]==False'
+        z1 = z1 - np.sum(xb[np.any(xb[:, a_vars] > 0, axis=1)], axis=0)
         tab = np.vstack((xb, z, z1))
-        if np.any(tab[-1, basics]):
-            for eq_i in range(len(xb)):
-                col_basic = basics[xb[eq_i, basics] > 0]
-                multiplier = [e*tab[-1, col_basic] for e in tab[eq_i]]
-                tab[-1] = [e1-e2 for e1,e2 in zip(tab[-1], multiplier)]
-        return np.array(tab).astype(Fraction)
+        return np.array(tab, dtype='float')
 
     def to_tableau_second_phase(self, tableau, a_vars):
         tab = np.delete(tableau, -1, 0)
@@ -163,13 +183,13 @@ class SIMPLEX:
 
     def can_be_improved(self, tableau):
         z = tableau[-1]
-        return any(x > self.TOL for x in z[:-1])
+        return any(x < -self.TOL for x in z[:-1])
 
-    def get_pivot_position(self, tableau, basics, debug):
-        if debug: print(tableau)
+    def get_pivot_position(self, tableau, basics):
+        if self.debug: [print(' \t'.join(map(str, i))) for i in tableau]
         z = tableau[-1]
-        # column_index = np.argmax(z[:-1])
-        column_index = next(i for i, x in enumerate(z[:-1]) if x > self.TOL)
+        column_index = np.argmin(z[:-1])
+        # column_index = next(i for i, x in enumerate(z[:-1]) if x > self.TOL)
 
         restrictions = []
         for equation in tableau[:len(basics)]:
@@ -180,20 +200,24 @@ class SIMPLEX:
         if min_ratio == math.inf:
             raise Exception("Unbounded")
         row_index = restrictions.index(min_ratio)
-        if debug: print("Pivot row", row_index, " col", column_index)
+        if self.debug: print("Pivot row", row_index, " col", column_index)
         return row_index, column_index
 
     def pivot_step(self, tableau, pivot_position):
         i, j = pivot_position
         pivot_value = tableau[i, j]
-        tableau[i] = [e/pivot_value for e in tableau[i]]
+        tableau[i] = [e / pivot_value for e in tableau[i]]
 
         for eq_i in range(len(tableau)):
             if eq_i != i:
                 m = tableau[eq_i, j]
-                multiplier = [e*m for e in tableau[i]]
-                tableau[eq_i] = [e1-e2 for e1, e2 in zip(tableau[eq_i], multiplier)]
-        return tableau
+                multiplier = [e * m for e in tableau[i]]
+                tableau[eq_i] = [e1 - e2 for e1, e2 in zip(tableau[eq_i], multiplier)]
+        norm = np.array([np.array([v if abs(round(v,1)-v)>self.TOL else round(v,1) for v in r]) for r in tableau])
+        if self.debug and not np.array_equal(tableau, norm):
+            print("HERE a DIFFERENCE")
+            print(tableau)
+        return norm
 
     def is_basic(self, column):
         return sum(column) == 1 and len([c for c in column if c == 0]) == len(column) - 1
@@ -211,9 +235,9 @@ class SIMPLEX:
         return solutions
 
     def get_objective_function_value(self, tableau):
-        return -tableau[-1][-1]
+        return -tableau[-1][-1]  # the change of sign is just to display better the solution
 
-    def solve(self, c, A, b, halfspaces=None, feasible_point=None, show=False, debug=False):
+    def solve(self, c, A, b, print_result=False, render=False):
         real_variables = len(A[0])
         c, A, b, art_variables, basic_variables = self.to_standard_form(c, A, b)
         tableau = self.to_tableau_first_phase(c, A, b, art_variables, basic_variables)
@@ -228,49 +252,44 @@ class SIMPLEX:
                 solutions.append(self.get_solution(tableau, basic_variables))
 
             while self.can_be_improved(tableau):
-                pivot_position = self.get_pivot_position(tableau, basic_variables, debug)
+                pivot_position = self.get_pivot_position(tableau, basic_variables)
                 tableau = self.pivot_step(tableau, pivot_position)
                 basic_variables[pivot_position[0]] = pivot_position[1]
                 zs.append(self.get_objective_function_value(tableau))
                 solutions.append(self.get_solution(tableau, basic_variables))
+                if phase1:
+                    if any([opt<0 for opt in np.array(solutions[-1])[art_variables]]):
+                        print("capture infeasible")  # TODO prova ad implementarlo (guarda geekrodion)
 
             if phase1:
                 phase1 = False
+                if any([art in basic_variables for art in art_variables]):
+                    raise Exception("mmmh")
                 if np.any(np.array(solutions[-1])[art_variables]):
                     raise Exception("Infeasible")
-                if debug:
-                    print("-------------------------------------")
+                if self.debug:
+                    [print(' \t'.join(map(str, i))) for i in tableau]
+                    print("--------------- End Phase 1 ---------------")
             elif phase2:
                 phase2 = False
 
-        if debug: print(tableau)
-        if debug: print("Solution values: ", solutions[-1])
-        if debug: print("Basic variables: ", basic_variables)
-        print("Solution: ", solutions[-1][:real_variables])
+        if self.debug: print(tableau)
+        if self.debug: print("Solution values: ", solutions[-1])
+        if self.debug: print("Basic variables: ", basic_variables)
+        if print_result: print("Solution: ", solutions[-1][:real_variables])
 
-        if show:
-            # for solutions rendering
-            points = [v[:2] for v in solutions]
-            xlim = (-1, max([p[0] for p in points]) + 1)
-            ylim = (-1, max([p[1] for p in points]) + 1)
-            render_inequalities(halfspaces, feasible_point, xlim, ylim)
-
-            for start, end in zip(points[:-1], points[1:]):
-                dx = end[0] - start[0]
-                dy = end[1] - start[1]
-                if dx > 0 or dy > 0:
-                    plt.arrow(start[0], start[1], dx, dy, width=0.1, length_includes_head=True, color='#1abc9c')
-            plt.show()
-
+        if render:
             # for degeneracy detection
             steps = range(len(zs))
             plt.plot(steps, zs, color="#2c3e50")
             plt.xticks(steps)
-            plt.xlabel('steps')
-            plt.ylabel('objective function value')
+            plt.xlabel('Steps')
+            plt.ylabel('Objective function value')
+            plt.title('Simplex')
             plt.show()
 
         return solutions[-1][:real_variables]
 
+
 if __name__ == '__main__':
-    SIMPLEX()
+    SIMPLEX(debug=True)
